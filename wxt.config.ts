be4aa192db38231ac34df03c81af9e655662019a
Chris Griffing <cmgriffing@gmail.com>
2025-11-27 13:51:31 -0800

    fix: clean up unused entrypoints, add proper logo, and add firefox specific manifest fixes

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,4 +4,21 @@ import { defineConfig } from "wxt";
 export default defineConfig({
   modules: ["@wxt-dev/module-react"],
   outDir: "dist",
+  manifest: {
+    name: "NPM Version Stats",
+    description:
+      "Show npm package version usage statistics inline on the npmjs.com package page in the versions tab",
+    version: "1.0.2",
+    permissions: ["activeTab"],
+    host_permissions: ["https://www.npmjs.com/package/*"],
+    manifest_version: 3,
+    browser_specific_settings: {
+      gecko: {
+        id: "@npm-version-stats",
+        data_collection_permissions: {
+          required: ["none"],
+        },
+      },
+    },
+  },
 });
